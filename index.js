1d98ab8831c5edd7e240e31561a3f2e86fc23222
Jeremy Tesh <jermitesh20@gmail.com>
2022-07-03 22:05:34 +0300

    simple meta mask connection script

---
+++ index.js
@@ -0,0 +1,9 @@
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    console.log("connected!!");
+    document.getElementById("connectButton").innerHTML = "Connected";
+  } else
+    document.getElementById("connectButton").innerHTML =
+      "Please install meta mask";
+}
